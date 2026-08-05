ebb4d611d4695ee894131e33e5d551dcf160a810
Matheus Carvalho <henriquematheus.c@gmail.com>
2026-08-05 17:36:12 -0300

    Setup inline css

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    inlineCss: true,
+  },
   output: "export",
   distDir: "dist",
   trailingSlash: true,
